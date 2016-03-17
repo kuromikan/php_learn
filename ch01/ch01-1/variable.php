3db518edf6b1a1ef2800c7960c93719164efce81
YAO,WEI <gamania54088@gmail.com>
2016-03-17 14:49:02 +0800

    ch01-1

---
+++ ch01/ch01-1/variable.php
@@ -0,0 +1,27 @@
+<?php
+
+$integer=123;
+echo "integer=".$integer."<br>";
+//宣告一個變數名稱為integer，值是123
+//在值給變數時沒有用"或'包起來或者是值沒有小數點的情況則被判斷為整數
+//echo則是php印出的其中一種語法，他可以印出變數或者字串組合
+//關於字串組合將在ch01-2介紹
+
+
+$character='char';
+echo "character=".$character."<br>";
+//宣告一個變數名稱為character，值是char
+//在值給變數若是使用'包起來該變數則為字元
+
+$string="str";
+echo "string=".$string."<br>";
+//宣告一個變數名稱為string，值是str
+//在值給變數若是使用"包起來該變數則為字串
+
+$float=12.3;
+echo "float=".$float."<br>";
+//宣告一個變數名稱為float，值是12.3
+//在值給變數時沒有用"或'包起來並且有小數點的情況則被判斷為浮點數
+
+
+?>
