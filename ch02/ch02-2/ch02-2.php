d94792c9f1f1aa51c9500f471df7b0f29f62cada
YAO,WEI <gamania54088@gmail.com>
2016-03-22 14:03:12 +0800

    ch02-2

--- ch02/ch02-2/ch02-2.php
+++ ch02/ch02-2/ch02-2.php
@@ -8,7 +8,7 @@ echo "num1=".$num1.",num2=".$num2."<br>";
 //宣告兩個變數一個值為10另外一個為10，值的部分可以隨意調整
 
 echo "<br>使用if(條件){}else{}<br>";
-echo "if(當num1=num2時){條件成立時執行程式}else{條件不成立時執行程式}<br>";
+echo "if(當num1=num2時){條件成立時印出num1等於num2}<br>else{條件不成立時印出num1不等於num2}<br><br>";
 if($num1==$num2)
 {
 	echo "num1等於num2<br>";
@@ -28,7 +28,7 @@ echo "<br>num1=".$num1.",num2=".$num2."<br>";
 //將num1設定為10、num2設定為5，值的部分可以隨意調整
 
 echo "<br>使用if(條件){}else{}<br>";
-echo "if(當num1=num2時){條件成立時執行程式}else{條件不成立時執行程式}<br>";
+echo "if(當num1=num2時){條件成立時印出num1等於num2}<br>else{條件不成立時印出num1不等於num2}<br><br>";
 if($num1==$num2)
 {
 	echo "num1等於num2<br>";
