02473059dabb0b0b46db39033be7774c950252d5
YAO,WEI <gamania54088@gmail.com>
2016-03-25 14:36:59 +0800

    readme

--- ch02/ch02-5/ch02-5.php
+++ ch02/ch02-5/ch02-5.php
@@ -10,7 +10,7 @@ echo "num1=".$num1.",num2=".$num2."<br>";
 $result=($num1==$num2?"num1等於num2<br>":"num1不等於num2");
 //宣告一個變數result來儲存三元運算後的值
 echo $result;
-//使用三元運算式(條件?程式1:程式2)
+//使用三元運算(條件?程式1:程式2)
 //如果num1=num2成立的話會將冒號左邊的值儲存至result
 //如果不成立則會將冒號右邊的值儲存至result
 
