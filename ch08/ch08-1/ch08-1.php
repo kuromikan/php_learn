98aeef0f807634c56ceda3992ed2a3cbc220777f
YAO,WEI <gamania54088@gmail.com>
2016-04-28 10:55:31 +0800

    ch08-1

--- ch08/ch08-1/ch08-1.php
+++ ch08/ch08-1/ch08-1.php
@@ -1,7 +1,12 @@
 <?php
 header("Content-Type:text/html; charset=utf-8");
 session_start();
+unset($_SESSION["time"]);
+echo "session_start<br>";
+echo "session設定前<br>";
+echo $_SESSION["time"]."<br>";
+echo "session設定後<br>";
 $_SESSION["time"]=date('Y-m-d h:i:s');
-echo $_SESSION["time"]
+echo $_SESSION["time"]."<br>";
 
 ?>
